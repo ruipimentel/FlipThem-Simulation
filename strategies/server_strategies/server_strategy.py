20e372bc33395602a8405f7ceb2865f84b880493
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-06-05 18:32:50 +0900

    Increments documentation on `ServerStrategy` class.

--- strategies/server_strategies/server_strategy.py
+++ strategies/server_strategies/server_strategy.py
@@ -1,10 +1,12 @@
+from typing import Dict
+
 class ServerStrategy:
-    def get_next_move_time(self, information):
+    def get_next_move_time(self, information: Dict) -> float:
         """
         :param information: Dictionary containing game information
         :return:
         """
         raise NotImplementedError("get_next_move_time method has not been implemented within strategy subclass")
 
-    def __str__(self):
+    def __str__(self) -> str:
         raise NotImplementedError("__str__ method has not been implemented within strategy subclass")
