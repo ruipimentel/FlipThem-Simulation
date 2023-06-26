4c9106e4eb91b04392b7f92afee76e555804f2c7
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-06-26 22:34:02 +0900

    Changes bitstring reading method to Gray Code.

--- util/bitstring.py
+++ util/bitstring.py
@@ -2,6 +2,8 @@ from typing import List, Literal
 
 import random
 
+import graycode
+
 def random_bitstring(string_size: int) -> List[Literal[0, 1]]:
     return [ random.randint(0, 1) for _ in range(string_size) ]
 
@@ -11,11 +13,14 @@ def read_bitstring(bitstring: List[Literal[0, 1]], min_value=None, max_value=Non
     `max_value` are not specified), or as a float (if values are supplied for both
     `min_value` and `max_value`).
 
-    The way it computes the float value is by mapping `0b0000...00` to `min_value`,
-    `0b1111...11` to `max_value`, and every integer along the way proportionately.
+    The way it reads the integer is through Gray Code.
+
+    The way it computes the float value is by mapping the lowest Gray Code to
+    `min_value`, the greatest to `max_value`, and the others proportionately.
     """
 
     integer_value = int(''.join([str(b) for b in bitstring]), 2)
+    integer_value = graycode.gray_code_to_tc(integer_value)
     if min_value is not None and max_value is not None:
         return min_value + (max_value - min_value)*integer_value/(2**len(bitstring) - 1)
     return integer_value
